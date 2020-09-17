c4f4f9cf03bb5e758240fd3e08678312f75165b1
dayan2222 <dayanahmed2222@gmail.com>
2020-09-17 13:02:43 +0500

    Forms 7 restful controllers

--- Laravel Views/dyanamic_data/app/Http/Controllers/ArticleController.php
+++ Laravel Views/dyanamic_data/app/Http/Controllers/ArticleController.php
@@ -7,17 +7,43 @@ use Illuminate\Http\Request;
 class ArticleController extends Controller
 {
  
-    public function index(Type $var = null)
+    public function index()
     {
-        # code...
+        # Render a list of a resource...
         $articles = Article::latest()->get();
         return view('articles.index',['articles'=>$articles]);
     }
 
     public function show($id)
     {
-        # code...
+        # Shows a single resource...
         $article = Article::find($id);
         return view('articles.show',['article'=>$article]);
     }
+
+    public function create()
+    {
+        # Shows a view to create a new resource...
+    }
+
+    public function store()
+    {
+        # Persist the new resource...
+    }
+
+    public function edit()
+    {
+        # Show a view to edit an existing resource...
+    }
+
+    public function update()
+    {
+        # Persist the edited resource...
+    }
+
+    public function destroy()
+    {
+        # Delete the resource...
+    }
+
 }
