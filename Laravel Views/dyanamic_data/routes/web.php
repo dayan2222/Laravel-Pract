c516327225c7c5ec288484d3bd19c7af4f7ad34e
dayan2222 <dayanahmed2222@gmail.com>
2020-09-22 00:59:51 +0500

    Submitting PUT Request

--- Laravel Views/dyanamic_data/routes/web.php
+++ Laravel Views/dyanamic_data/routes/web.php
@@ -30,5 +30,7 @@ Route::get('/articles','\App\Http\Controllers\ArticleController@index');
 Route::post('/articles','App\Http\Controllers\ArticleController@store');
 Route::get('/articles/create','\App\Http\Controllers\ArticleController@create');
 Route::get('/articles/{article}','\App\Http\Controllers\ArticleController@show');
+Route::get('/articles/{article}/edit','\App\Http\Controllers\ArticleController@edit');
+Route::put('/articles/{article}','\App\Http\Controllers\ArticleController@update');
 
 
